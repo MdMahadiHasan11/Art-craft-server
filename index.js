82ea48be064b0099719f010ba436b05758b0c96b
MdMahadiHasan11 <hasanpustcse11@gmail.com>
2024-04-29 15:37:46 +0600

    category item added and get operation done

--- index.js
+++ index.js
@@ -31,28 +31,79 @@ async function run() {
         // const database = client.db("usersDB");
         // const usersCollection = database.collection("users");
         const usersCollection = client.db("allCraftDB").collection("craft");
+        
+        
+        
 
 
-        app.get('/addCrafts',async( req, res)=> {
+
+        // category
+
+        const categoryCollection = client.db("categoryBD").collection("category");
+
+        app.get('/category',async( req, res)=> {
+            const cursor = categoryCollection.find()
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+        // category
+
+
+
+
+
+
+        app.get('/addCrafts', async (req, res) => {
             const cursor = usersCollection.find()
             const result = await cursor.toArray();
             res.send(result);
         })
 
-       
+        app.get('/myCraft/:email', async (req, res) => {
+            // const cursor = usersCollection.find()
+            const result = await usersCollection.find({ email: req.params.email }).toArray();
+            res.send(result);
+        })
+
+        // single product update /details
+        app.get('/singleProduct/:id', async (req, res) => {
+            // const cursor = usersCollection.find()
+            const result = await usersCollection.findOne({ _id: new ObjectId(req.params.id), })
+            res.send(result);
+        })
+
+        // details
+        app.get('/details/:id', async (req, res) => {
+            // const cursor = usersCollection.find()
+            const result = await usersCollection.findOne({ _id: new ObjectId(req.params.id), })
+            res.send(result);
+        })
 
 
-        app.put('/updateProduct/:id',async(req,res)=>{
+
+        app.post('/addCrafts', async (req, res) => {
+            const newCraft = req.body;
+            console.log(newCraft);
+            const result = await usersCollection.insertOne(newCraft);
+            res.send(result);
+
+
+
+        }
+        )
+
+
+        app.put('/updateProduct/:id', async (req, res) => {
             const id = req.params.id;
             const user = req.body;
             console.log(id, user);
-            const filter = {_id:new ObjectId(id)}
-            const options = {upsert:true}
-            const updateUser={
-                $set:{
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true }
+            const updateUser = {
+                $set: {
 
                     // const updateCraft = { displayName, email, item_name, subcategoryName, description, price, rating, processingTime, customization, stockStatus, image }
-                    
+
                     item_name: user.item_name,
                     subcategoryName: user.subcategoryName,
                     description: user.description,
@@ -66,17 +117,17 @@ async function run() {
                 }
             }
 
-            const result = await usersCollection.updateOne(filter,updateUser,options);
+            const result = await usersCollection.updateOne(filter, updateUser, options);
             res.send(result);
 
         })
 
-        app.delete('/delete/:id',async(req,res)=>{
+        app.delete('/delete/:id', async (req, res) => {
             const id = req.params.id;
 
             console.log('delete form database ', id);
 
-            const query = {_id: new ObjectId(id) }
+            const query = { _id: new ObjectId(id) }
             const result = await usersCollection.deleteOne(query);
             res.send(result);
 
@@ -93,9 +144,6 @@ async function run() {
     }
 }
 run().catch(console.dir);
-
-
-
 
 
 app.get("/", (req, res) => {
